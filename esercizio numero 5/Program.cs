1b2c84bf2168012c0272f8a7337fcad235a8f47f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 13:14:31 +0200

    fine

--- esercizio numero 5/Program.cs
+++ esercizio numero 5/Program.cs
@@ -6,7 +6,17 @@ namespace esercizio_numero_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("inserisci la temperatura della serra");
+            double temperatura = double.Parse(Console.ReadLine());
+            if (temperatura >= 18)
+            {
+                double t = (temperatura + 18);
+                double temperaturainiziale = temperatura + 18;
+                double temperaturafinale = temperatura - 5;
+                double temperaturamedia = temperatura - 5 + 18;
+
+            }
+            Console.ReadLine();
         }
     }
 }
