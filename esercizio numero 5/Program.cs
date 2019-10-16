de9a2f3e03bc05132178ac494e38c2c53b1bfced
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:41:58 +0200

    Add project files.

---
+++ esercizio numero 5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace esercizio_numero_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
